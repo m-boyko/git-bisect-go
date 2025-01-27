b6da2a3c2de8b0fddc30d1d82c2bd42c2cb386c8
Margarita Boyko <m-boyko@outlook.com>
2025-01-27 15:19:03 +0300

    2 commit

--- add.go
+++ add.go
@@ -6,6 +6,11 @@ func Add(a, b int) int {
 	return a + b
 }
 
+func Sub(a, b int) int {
+	return a - b
+}
+
 func main() {
 	fmt.Println(Add(1, 2))
+	fmt.Println(Sub(1, 2))
 }
--- add_test.go
+++ add_test.go
@@ -8,3 +8,10 @@ func TestAdd(t *testing.T) {
 		t.Errorf("Expected 3, but got %d", result)
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := Sub(1, 2)
+	if result != -1 {
+		t.Errorf("Expected -1, but got %d", result)
+	}
+}
